7c72f6b8cf786a0844b695154edead6acb362155
Aarya Shrestha <pranishashrestha27@gmail.com>
2026-02-11 12:25:17 +0545

    Update mentor name for Art Club in SQL insert

--- inserting_data.sql
+++ inserting_data.sql
@@ -4,7 +4,7 @@ VALUES (8, 'Kiran', 'kiran@email.com');
 
 -- Insert a new club
 INSERT INTO Club (ClubID, ClubName, ClubRoom, ClubMentor) 
-VALUES (5, 'Art Club', 'R404', 'Ms. Maya');
+VALUES (5, 'Art Club', 'R404', 'Ms. Meera');
 
 -- Display all students
 SELECT * FROM Student;
